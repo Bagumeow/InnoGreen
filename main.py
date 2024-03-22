4d279149929ee98067380255fa3ba1728ca60972
goiliace <zombizombi51@gmail.com>
2024-03-22 19:04:50 +0700

    ok

--- main.py
+++ main.py
@@ -2,10 +2,39 @@ from tools.tools import *
 from fastapi import FastAPI
 from router import  users,current_user
 import os
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, __version__
+from fastapi.responses import HTMLResponse
 from dotenv import load_dotenv
+
+
 load_dotenv('.env')
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI@{__version__}</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
 @app.get("/")
 def router():
     return {"message": "API from Dumplings team project"}
